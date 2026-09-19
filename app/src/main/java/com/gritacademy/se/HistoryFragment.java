4134f8266e557bea1af7e88e71a6b9eedc54342e
andreitamas <andrei.tamass83@gmail.com>
2026-09-19 18:57:03 +0200

    added history layout with working code gathering firbase database to code, displaying it in history fragment

--- app/src/main/java/com/gritacademy/se/HistoryFragment.java
+++ app/src/main/java/com/gritacademy/se/HistoryFragment.java
@@ -5,6 +5,10 @@ import android.view.View;
 
 import androidx.fragment.app.Fragment;
 
+import android.widget.TextView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
 public class HistoryFragment extends Fragment {
 
     public HistoryFragment() {
@@ -14,5 +18,30 @@ public class HistoryFragment extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        TextView historyText = view.findViewById(R.id.historyText);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("weatherHistory")
+                .get()
+                .addOnSuccessListener(documents -> {
+
+                    String history = "";
+
+                    for (QueryDocumentSnapshot document : documents) {
+                        history += document.getString("city") + "\n";
+                        history += document.getDouble("temperature") + " °C\n";
+                        history += document.getDouble("wind") + " m/s\n";
+                        //history += "---------------------";
+                    }
+
+                    historyText.setText(history);
+
+                });
+
+
+
+
     }
 }
--- app/src/main/java/com/gritacademy/se/MenuFragment.java
+++ app/src/main/java/com/gritacademy/se/MenuFragment.java
@@ -19,14 +19,14 @@ public class MenuFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         Button weatherButton = view.findViewById(R.id.weatherButton);
-        Button currencyButton = view.findViewById(R.id.historyButton);
+        Button historyButton = view.findViewById(R.id.historyButton);
 
         weatherButton.setOnClickListener(v -> openWeather());
 
-        currencyButton.setOnClickListener(v -> openCurrency());
+        historyButton.setOnClickListener(v -> openHistory());
     }
 
-    private void openCurrency() {
+    private void openHistory() {
         requireActivity().getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.fragment_container, new HistoryFragment())
